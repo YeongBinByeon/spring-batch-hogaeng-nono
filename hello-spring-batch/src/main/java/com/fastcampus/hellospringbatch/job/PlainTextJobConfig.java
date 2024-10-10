491bb8635584957355b425aea5314b2a9afa3d2f
yeongbin <yeongbin6483@gmail.com>
2024-10-10 09:28:32 +0900

    03. 데이터를 읽고, 처리하고, 쓰기

---
+++ hello-spring-batch/src/main/java/com/fastcampus/hellospringbatch/job/PlainTextJobConfig.java
@@ -0,0 +1,91 @@
+package com.fastcampus.hellospringbatch.job;
+
+import com.fastcampus.hellospringbatch.core.domain.PlainText;
+import com.fastcampus.hellospringbatch.core.repository.PlainTextRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class PlainTextJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final PlainTextRepository plainTextRepository;
+
+    @Bean("plainTextJob")
+    public Job plainTextJob(Step plainTextStep){
+        return jobBuilderFactory.get("plainTextJob")
+                .incrementer(new RunIdIncrementer())
+                .start(plainTextStep)
+                .build();
+    }
+
+    @JobScope
+    @Bean("plainTextStep")
+    public Step plainTextStep(ItemReader plainTextReader, ItemProcessor plainTextProcessor,
+                              ItemWriter plainTextWriter){
+        return stepBuilderFactory.get("plainTextStep")
+                .<PlainText, String>chunk(5)
+                .reader(plainTextReader)
+                .processor(plainTextProcessor)
+                .writer(plainTextWriter)
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public RepositoryItemReader<PlainText> plainTextReader(){
+        return new RepositoryItemReaderBuilder<PlainText>()
+                .name("plainTextReader")
+                .repository(plainTextRepository)
+                .methodName("findBy")
+                .pageSize(5)
+                .arguments(List.of())
+                .sorts(Collections.singletonMap("id", Sort.Direction.DESC))
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public ItemProcessor<PlainText, String> plainTextProcessor(){
+        return new ItemProcessor<PlainText, String>() {
+            @Override
+            public String process(PlainText item) throws Exception {
+                return "processed " + item.getText();
+            }
+        };
+    }
+
+    @StepScope
+    @Bean
+    public ItemWriter<String> plainTextWriter(){
+        return new ItemWriter<String>() {
+            @Override
+            public void write(List<? extends String> items) throws Exception {
+                items.forEach(System.out::println);
+                System.out.println("==== chunk is finished");
+            }
+        };
+    }
+
+}
