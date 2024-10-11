034d390d55fbd75d1fbeeaef21c0763396113a53
yeongbin <yeongbin6483@gmail.com>
2024-10-11 13:23:16 +0900

    ch 04. / 03. FlatFileItemReader

---
+++ hello-spring-batch/src/main/java/com/fastcampus/hellospringbatch/dto/PlayerDto.java
@@ -0,0 +1,15 @@
+package com.fastcampus.hellospringbatch.dto;
+
+import lombok.Data;
+
+@Data
+public class PlayerDto {
+
+    private String ID;
+    private String lastName;
+    private String firstName;
+    private String position;
+    private int birthYear;
+    private int debutYear;
+
+}
