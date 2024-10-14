6f011636bd03e2b0178aff1b3b9441b984d3c5a5
yeongbin <yeongbin6483@gmail.com>
2024-10-14 21:27:22 +0900

    호갱노노 배치 시스템 만들기 / 01. 요구사항 분석하기

---
+++ house-batch/src/main/java/com/fastcampus/house_batch/HouseBatchApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.house_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseBatchApplication.class, args);
+	}
+
+}
